706b2ef89a3da6dbad2f696ce0fff46a15746e50
prateekshukla17 <4944.prateek@gmail.com>
2025-09-11 03:57:41 +0530

    fixed prisma client generation for all three dbs and created the shared global db point

---
+++ shared/database.js
@@ -0,0 +1,36 @@
+const {
+  PrismaClient: CustomerPrismaClient,
+} = require('../../node_modules/.prisma/customer-client');
+const {
+  PrismaClient: CampaignPrismaClient,
+} = require('../../node_modules/.prisma/campaign-client');
+const {
+  PrismaClient: DeliveryPrismaClient,
+} = require('../../node_modules/.prisma/delivery-client');
+
+const customerPrisma = new CustomerPrismaClient();
+const campaignPrisma = new CampaignPrismaClient();
+const deliveryPrisma = new DeliveryPrismaClient();
+
+module.exports = {
+  customerDB: {
+    prisma: customerPrisma,
+    customer: customerPrisma.customer,
+    order: customerPrisma.order,
+    outboxEvent: customerPrisma.outboxEvent,
+  },
+
+  campaignDB: {
+    prisma: campaignPrisma,
+    customerMV: campaignPrisma.customerMV,
+    segment: campaignPrisma.segment,
+    campaign: campaignPrisma.campaign,
+    campaignStats: campaignPrisma.campaignStats,
+  },
+
+  deliveryDB: {
+    prisma: deliveryPrisma,
+    communicationLog: deliveryPrisma.communicationLog,
+    deliveryReceipt: deliveryPrisma.deliveryReceipt,
+  },
+};

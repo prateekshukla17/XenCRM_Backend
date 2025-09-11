e18483c96b039d7799b218f618e04a5b82507942
prateekshukla17 <4944.prateek@gmail.com>
2025-09-11 14:48:35 +0530

    wrote ingestion logic for rabbit mq

--- services/Customer/controllers/customer.js
+++ services/Customer/controllers/customer.js
@@ -1,1 +1,3 @@
 const { customerDB } = require('../../../shared/database');
+
+const customer = async (req, res) => {};
--- shared/utils/rabbitmq.js
+++ shared/utils/rabbitmq.js
@@ -37,5 +37,51 @@ class RabbitMQ {
       setTimeout(() => this.connect(), 5000);
     }
   }
+
+  async setupIngestion_Exchanges() {
+    try {
+      if (!this.channel) return;
+      const ingestionExchange = 'data_ingestion';
+      await this.channel.assertExchange(ingestionExchange, 'direct', {
+        durable: true,
+      });
+      const customerQueue = 'customer_ingestion_queue';
+      await this.channel.assertQueue(customerQueue, {
+        durable: true,
+        arguments: {
+          'x-message-ttl': 86400000,
+        },
+      });
+
+      await this.channel.bindQueue(
+        customerQueue,
+        ingestionExchange,
+        'customer'
+      );
+
+      const ordersQueue = 'order_ingestion_queue';
+      await this.channel.assertQueue(ordersQueue, {
+        durable: true,
+        arguments: {
+          'x-message-ttl': 86400000,
+        },
+      });
+
+      await this.channel.bindQueue(ordersQueue, ingestionExchange, 'order');
+
+      console.log('Ingestion Infra setup complete');
+      console.log('Exchange: Data_ingestions');
+      console.log(
+        '- Customer Queue: customer_ingestion_queue (routing key: customer)'
+      );
+      console.log(
+        '- Orders Queue: orders_ingestion_queue (routing key: order)'
+      );
+    } catch (error) {
+      console.error('Failed to setup Ingestion Infra:', error);
+      throw error;
+    }
+  }
 }
+
 module.exports = new RabbitMQ();
